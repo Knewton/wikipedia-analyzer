77534cc83f488ce613de41e0c12847659510d55d
Antony Van der Mude <vandermude@knewton.com>
2014-12-11 13:49:45 -0500

    Created test-pages directory
    Code and data for mathematics wiki pages

---
+++ test-pages/math-topics.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python
+"""
+This program takes the topics names in the Knewton ontology and finds related Wikipedia pages
+These pages will form the training set
+"""
+import wikipedia
+
+def main():
+    print "TOPIC: Algebra & Algebra II"
+    print wikipedia.search("Algebra & Algebra II")
+    print "TOPIC: Exponents, Radicals, and Logarithmic Equations & Functions"
+    print wikipedia.search("Exponents, Radicals, and Logarithmic Equations & Functions")
+    print "TOPIC: Exponential Equations and Functions"
+    print wikipedia.search("Exponential Equations and Functions")
+    print "TOPIC: Imaginary and complex numbers"
+    print wikipedia.search("Imaginary and complex numbers")
+    print "TOPIC: Logarithms"
+    print wikipedia.search("Logarithms")
+    print "TOPIC: Properties of Logarithms"
+    print wikipedia.search("Properties of Logarithms")
+    print "TOPIC: Solving Logarithmic Equations"
+    print wikipedia.search("Solving Logarithmic Equations")
+    print "TOPIC: Pythagorean Theorem"
+    print wikipedia.search("Pythagorean Theorem")
+    print "TOPIC: Radicals"
+    print wikipedia.search("Radicals")
+    print "TOPIC: Operations with radical expressions"
+    print wikipedia.search("Operations with radical expressions")
+    print "TOPIC: Radicals with index > 2"
+    print wikipedia.search("Radicals with index > 2")
+    print "TOPIC: Rationalizing radicals in the denominator"
+    print wikipedia.search("Rationalizing radicals in the denominator")
+    print "TOPIC: Simplifying radical expressions"
+    print wikipedia.search("Simplifying radical expressions")
+    print "TOPIC: Solving and analyzing radical functions"
+    print wikipedia.search("Solving and analyzing radical functions")
+    print "TOPIC: Simplifying Exponents"
+    print wikipedia.search("Simplifying Exponents")
+    print "TOPIC: Linear Equations, Expressions, Inequalities & Functions"
+    print wikipedia.search("Linear Equations, Expressions, Inequalities & Functions")
+    print "TOPIC: Absolute Value Equations and Inequalities"
+    print wikipedia.search("Absolute Value Equations and Inequalities")
+    print "TOPIC: Domain and Range"
+    print wikipedia.search("Domain and Range")
+    print "TOPIC: Equation of a Line"
+    print wikipedia.search("Equation of a Line")
+    print "TOPIC: Evaluating Functions"
+    print wikipedia.search("Evaluating Functions")
+    print "TOPIC: Evaluating algebraic expressions"
+    print wikipedia.search("Evaluating algebraic expressions")
+    print "TOPIC: Graphing Linear Equations"
+    print wikipedia.search("Graphing Linear Equations")
+    print "TOPIC: Graphing Linear Inequalities"
+    print wikipedia.search("Graphing Linear Inequalities")
+    print "TOPIC: Inverse of Functions"
+    print wikipedia.search("Inverse of Functions")
+    print "TOPIC: Literal equations"
+    print wikipedia.search("Literal equations")
+    print "TOPIC: Operations on Functions"
+    print wikipedia.search("Operations on Functions")
+    print "TOPIC: Piecewise Functions"
+    print wikipedia.search("Piecewise Functions")
+    print "TOPIC: Relations and Functions"
+    print wikipedia.search("Relations and Functions")
+    print "TOPIC: Simplifying Expressions"
+    print wikipedia.search("Simplifying Expressions")
+    print "TOPIC: Slope & Equations of a line"
+    print wikipedia.search("Slope & Equations of a line")
+    print "TOPIC: Slope of a Line"
+    print wikipedia.search("Slope of a Line")
+    print "TOPIC: Solving Single Variable Equations"
+    print wikipedia.search("Solving Single Variable Equations")
+    print "TOPIC: Systems of Linear Equations & Inequalities"
+    print wikipedia.search("Systems of Linear Equations & Inequalities")
+    print "TOPIC: Understanding single variable inequalities"
+    print wikipedia.search("Understanding single variable inequalities")
+    print "TOPIC: Writing algebraic equations"
+    print wikipedia.search("Writing algebraic equations")
+    print "TOPIC: Writing algebraic expressions"
+    print wikipedia.search("Writing algebraic expressions")
+    print "TOPIC: Matrix Algebra"
+    print wikipedia.search("Matrix Algebra")
+    print "TOPIC: Determinants and Cramers Rule"
+    print wikipedia.search("Determinants and Cramers Rule")
+    print "TOPIC: Matrix Operations"
+    print wikipedia.search("Matrix Operations")
+    print "TOPIC: Properties of Matrices"
+    print wikipedia.search("Properties of Matrices")
+    print "TOPIC: Quadratics & Polynomials: Equations, Functions & Factoring"
+    print wikipedia.search("Quadratics & Polynomials: Equations, Functions & Factoring")
+    print "TOPIC: Adding and Subtracting Polynomials"
+    print wikipedia.search("Adding and Subtracting Polynomials")
+    print "TOPIC: Comparing Linear, Quadratic, and Exponential Functions"
+    print wikipedia.search("Comparing Linear, Quadratic, and Exponential Functions")
+    print "TOPIC: Dividing Polynomials"
+    print wikipedia.search("Dividing Polynomials")
+    print "TOPIC: Factoring Quadratics"
+    print wikipedia.search("Factoring Quadratics")
+    print "TOPIC: Factoring polynomials"
+    print wikipedia.search("Factoring polynomials")
+    print "TOPIC: Multiplying Polynomials"
+    print wikipedia.search("Multiplying Polynomials")
+    print "TOPIC: Solving Polynomial Equations Degree > 2"
+    print wikipedia.search("Solving Polynomial Equations Degree > 2")
+    print "TOPIC: Solving Quadratic Equations and Inequalities"
+    print wikipedia.search("Solving Quadratic Equations and Inequalities")
+    print "TOPIC: Using the Discriminant"
+    print wikipedia.search("Using the Discriminant")
+    print "TOPIC: Rational Expressions, Equations & Functions"
+    print wikipedia.search("Rational Expressions, Equations & Functions")
+    print "TOPIC: Direct and Inverse Variation"
+    print wikipedia.search("Direct and Inverse Variation")
+    print "TOPIC: Domain of Rational Functions"
+    print wikipedia.search("Domain of Rational Functions")
+    print "TOPIC: Operations with Rational Expressions"
+    print wikipedia.search("Operations with Rational Expressions")
+    print "TOPIC: Simplifying Rational Expressions"
+    print wikipedia.search("Simplifying Rational Expressions")
+    print "TOPIC: Solving Rational Equations and Inequalities"
+    print wikipedia.search("Solving Rational Equations and Inequalities")
+    print "TOPIC: Data Analysis, Probability, & Statistics"
+    print wikipedia.search("Data Analysis, Probability, & Statistics")
+    print "TOPIC: Data Analysis & Statistics"
+    print wikipedia.search("Data Analysis & Statistics")
+    print "TOPIC: Measures of Central Tendency"
+    print wikipedia.search("Measures of Central Tendency")
+    print "TOPIC: Measures of Dispersion"
+    print wikipedia.search("Measures of Dispersion")
+    print "TOPIC: Samples and Populations"
+    print wikipedia.search("Samples and Populations")
+    print "TOPIC: Data Representation"
+    print wikipedia.search("Data Representation")
+    print "TOPIC: Bar graph"
+    print wikipedia.search("Bar graph")
+    print "TOPIC: Box and whiskers plot"
+    print wikipedia.search("Box and whiskers plot")
+    print "TOPIC: Circle graph"
+    print wikipedia.search("Circle graph")
+    print "TOPIC: Line graph"
+    print wikipedia.search("Line graph")
+    print "TOPIC: Stem and leaf plot"
+    print wikipedia.search("Stem and leaf plot")
+    print "TOPIC: Venn diagram"
+    print wikipedia.search("Venn diagram")
+    print "TOPIC: Probability"
+    print wikipedia.search("Probability")
+    print "TOPIC: Combinations"
+    print wikipedia.search("Combinations")
+    print "TOPIC: Outcomes and Events"
+    print wikipedia.search("Outcomes and Events")
+    print "TOPIC: Permutations"
+    print wikipedia.search("Permutations")
+    print "TOPIC: Geometry & Trigonometry"
+    print wikipedia.search("Geometry & Trigonometry")
+    print "TOPIC: 2-Dimensional Shapes"
+    print wikipedia.search("2-Dimensional Shapes")
+    print "TOPIC: Area"
+    print wikipedia.search("Area")
+    print "TOPIC: Circles"
+    print wikipedia.search("Circles")
+    print "TOPIC: Perimeter"
+    print wikipedia.search("Perimeter")
+    print "TOPIC: Polygons"
+    print wikipedia.search("Polygons")
+    print "TOPIC: Congruent Figures and Similar Figures"
+    print wikipedia.search("Congruent Figures and Similar Figures")
+    print "TOPIC: Triangles"
+    print wikipedia.search("Triangles")
+    print "TOPIC: 3-Dimensional Shapes and Solids"
+    print wikipedia.search("3-Dimensional Shapes and Solids")
+    print "TOPIC: Cone"
+    print wikipedia.search("Cone")
+    print "TOPIC: Cylinder"
+    print wikipedia.search("Cylinder")
+    print "TOPIC: Prism"
+    print wikipedia.search("Prism")
+    print "TOPIC: Pyramid"
+    print wikipedia.search("Pyramid")
+    print "TOPIC: Sphere"
+    print wikipedia.search("Sphere")
+    print "TOPIC: Surface Area"
+    print wikipedia.search("Surface Area")
+    print "TOPIC: Volume"
+    print wikipedia.search("Volume")
+    print "TOPIC: Coordinate and Analytic Geometry"
+    print wikipedia.search("Coordinate and Analytic Geometry")
+    print "TOPIC: Cartesian Coordinate Plane"
+    print wikipedia.search("Cartesian Coordinate Plane")
+    print "TOPIC: Distance Formula"
+    print wikipedia.search("Distance Formula")
+    print "TOPIC: Transformations"
+    print wikipedia.search("Transformations")
+    print "TOPIC: Dilations"
+    print wikipedia.search("Dilations")
+    print "TOPIC: Reflections"
+    print wikipedia.search("Reflections")
+    print "TOPIC: Rotations"
+    print wikipedia.search("Rotations")
+    print "TOPIC: Translations"
+    print wikipedia.search("Translations")
+    print "TOPIC: Points, Lines, Segments, Rays, Angles and Planes"
+    print wikipedia.search("Points, Lines, Segments, Rays, Angles and Planes")
+    print "TOPIC: Angles"
+    print wikipedia.search("Angles")
+    print "TOPIC: Angles formed by parallel lines and a transversal"
+    print wikipedia.search("Angles formed by parallel lines and a transversal")
+    print "TOPIC: Complementary Angles"
+    print wikipedia.search("Complementary Angles")
+    print "TOPIC: Supplementary Angles"
+    print wikipedia.search("Supplementary Angles")
+    print "TOPIC: Segments"
+    print wikipedia.search("Segments")
+    print "TOPIC: Trigonometry"
+    print wikipedia.search("Trigonometry")
+    print "TOPIC: Trigonometric Functions"
+    print wikipedia.search("Trigonometric Functions")
+    print "TOPIC: Sine, Cosine, and Tangent"
+    print wikipedia.search("Sine, Cosine, and Tangent")
+    print "TOPIC: Numbers & Computation"
+    print wikipedia.search("Numbers & Computation")
+    print "TOPIC: Number Concepts"
+    print wikipedia.search("Number Concepts")
+    print "TOPIC: Absolute Value"
+    print wikipedia.search("Absolute Value")
+    print "TOPIC: Real Numbers"
+    print wikipedia.search("Real Numbers")
+    print "TOPIC: Operations"
+    print wikipedia.search("Operations")
+    print "TOPIC: Addition and Subtraction"
+    print wikipedia.search("Addition and Subtraction")
+    print "TOPIC: Estimation"
+    print wikipedia.search("Estimation")
+    print "TOPIC: Exponents"
+    print wikipedia.search("Exponents")
+    print "TOPIC: Multiplication and Division"
+    print wikipedia.search("Multiplication and Division")
+    print "TOPIC: Order of Operations"
+    print wikipedia.search("Order of Operations")
+    print "TOPIC: Scientific Notation"
+    print wikipedia.search("Scientific Notation")
+    print "TOPIC: Square Roots"
+    print wikipedia.search("Square Roots")
+    print "TOPIC: Patterns, Sequences, & Series"
+    print wikipedia.search("Patterns, Sequences, & Series")
+    print "TOPIC: Arithmetic Sequence"
+    print wikipedia.search("Arithmetic Sequence")
+    print "TOPIC: Geometric Sequence"
+    print wikipedia.search("Geometric Sequence")
+    print "TOPIC: Recursive Sequence"
+    print wikipedia.search("Recursive Sequence")
+    print "TOPIC: Rules-Based"
+    print wikipedia.search("Rules-Based")
+    print "TOPIC: Series"
+    print wikipedia.search("Series")
+    print "TOPIC: Rates, Ratios & Proportions"
+    print wikipedia.search("Rates, Ratios & Proportions")
+    print "TOPIC: Percents"
+    print wikipedia.search("Percents")
+    print "TOPIC: Proportions"
+    print wikipedia.search("Proportions")
+    print "TOPIC: Rates"
+    print wikipedia.search("Rates")
+    print "TOPIC: Ratios"
+    print wikipedia.search("Ratios")
+
+if __name__ == '__main__':
+    main()
