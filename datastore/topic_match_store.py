17279060e51f3ccc3ba6c4843cb56d1d17e71128
Alex Heitzmann <alex.heitzmann@knewton.com>
2014-12-11 19:41:22 -0500

    Implement sqlite persistence helper with tests

--- datastore/topic_match_store.py
+++ datastore/topic_match_store.py
@@ -1,15 +1,45 @@
 import sqlite3
-
+import uuid
 
 class TopicMatchStore:
     """A helper for persisting matching topic/url pairs to SqLite"""
+    _TABLE_NAME = "TopicMatches"
+    _TOPIC_COL = "topic"
+    _URL_COL = "url"
+    _CONFIDENCE_COL = "confidence"
 
     def __init__(self, sqlite_file):
         """
         Initializer.
         :param string sqlite_file: Path to the file to use as the sqlite store.
+        ':memory:' may be passed if an in-memory store is desired.
         """
         self._db = sqlite3.connect(sqlite_file)
+        cursor = self._db.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS {}
+            (
+                id TEXT PRIMARY KEY,
+                {} TEXT,
+                {} TEXT,
+                {} REAL
+            )
+            '''.format(TopicMatchStore._TABLE_NAME,
+                       TopicMatchStore._TOPIC_COL,
+                       TopicMatchStore._URL_COL,
+                       TopicMatchStore._CONFIDENCE_COL))
+        self._db.commit()
+
+    def _does_match_exist(self, topic, url):
+        """
+        :param str topic: A string uniquely identifying a topic
+        :param str url: The potentially matching url
+        :return bool: True if a the given topic has the given url as a match in the db.
+        """
+        cursor = self._db.cursor()
+        cursor.execute("SELECT * FROM {} WHERE topic=? AND url=?"
+                       .format(TopicMatchStore._TABLE_NAME), (topic, url))
+        return cursor.fetchone() is not None
 
     def add_or_update_match(self, topic, url, confidence):
         """
@@ -21,7 +51,15 @@ class TopicMatchStore:
         the confidence in the match, 1 being more confident.
         :return: None
         """
-        pass
+        cursor = self._db.cursor()
+        if self._does_match_exist(topic, url):
+            cursor.execute("UPDATE {} SET confidence=? WHERE topic=? AND url=?"
+                           .format(TopicMatchStore._TABLE_NAME), (confidence, topic, url))
+        else:
+            id = str(uuid.uuid4())
+            cursor.execute("INSERT INTO {} VALUES (?, ?, ?, ?)"
+                           .format(TopicMatchStore._TABLE_NAME), (id, topic, url, confidence))
+        self._db.commit()
 
     def remove_match(self, topic, url):
         """
@@ -30,7 +68,27 @@ class TopicMatchStore:
         :param str url: The url to remove
         :return:
         """
-        pass
+        cursor = self._db.cursor()
+        cursor.execute("DELETE FROM {} WHERE topic=? AND url=?"
+                       .format(TopicMatchStore._TABLE_NAME), (topic, url))
+        self._db.commit()
+
+    def get_confidence(self, topic, url):
+        """
+        Get the confidence of a match.
+        :param topic: The topic of the match
+        :param url: The matching url
+        :return float: The confidence of the match
+        """
+        cursor = self._db.cursor()
+        cursor.execute("SELECT {} FROM {} WHERE topic=? AND url=?"
+                       .format(TopicMatchStore._CONFIDENCE_COL, TopicMatchStore._TABLE_NAME),
+                       (topic, url))
+        confidence = cursor.fetchone()
+        if confidence is None:
+            raise ValueError("No match found for topic {} url {}".format(topic, url))
+        return float(confidence[0])
+
 
     def get_matches(self, topic, threshold=0):
         """
@@ -39,10 +97,11 @@ class TopicMatchStore:
         :param float threshold: The threshold confidence level for
         returned matches; no match with confidence under the specified
         amount will be returned.
-        :return: A list of matching urls, sorted in decreasing order
+        :return: An iterable of matching urls, sorted in decreasing order
         of match confidence.
         """
-        pass
+        matches = self.get_matches_with_confidence(topic, threshold)
+        return (match[0] for match in matches)
 
     def get_matches_with_confidence(self, topic, threshold=0):
         """
@@ -51,7 +110,17 @@ class TopicMatchStore:
         :param float threshold: The threshold confidence level for
         returned matches; no match with confidence under the specified
         amount will be returned.
-        :return: A list (matching url, confidence) tuples, sorted in
+        :return: An iterable over (matching url, confidence) tuples, sorted in
         decreasing order of match confidence.
         """
-        pass
+        cursor = self._db.cursor()
+        return cursor.execute("SELECT {0},{1} FROM {2} WHERE {3}=? AND {1}>=? ORDER BY {1} DESC"
+                       .format(TopicMatchStore._URL_COL,
+                               TopicMatchStore._CONFIDENCE_COL,
+                               TopicMatchStore._TABLE_NAME,
+                               TopicMatchStore._TOPIC_COL),
+                       (topic, threshold))
+
+    def close(self):
+        """Close connection to the db"""
+        self._db.close()
