504bea74f205d5db94d0f947b704729eba2e5d2f
Miroslava Sotakova <mirka@knewton.com>
2014-12-12 19:37:48 -0500

    Improving the category/relevant link script
    The output is now in two files (if file output is desired):
    1. _categories.txt file is listing the relevant categories per taxonomy path
    2. _relevant_links.txt is listing relevant wikipedia page titles per
    taxonomy path
    
    I also added data responses I got for the retail math taxonomy forest.
    (For each taxonomy path, 10 search results were examined, 3 of the best
    based on categorization chosen for the best category matches, and the links
    they shared for the best link suggestions)
    
    TODO: Enhance the relevant link file with urls. Remove comma as a delimiter.

--- pagefetcher/get_wiki_categories.py
+++ pagefetcher/get_wiki_categories.py
@@ -19,6 +19,7 @@ import os
 import re
 import wikipedia
 
+from collections import defaultdict
 from wikipedia.exceptions import DisambiguationError
 from wikipedia.exceptions import PageError
 
@@ -29,9 +30,11 @@ FIRST_PASS_RESULT_COUNT = 20
 BEST_MATCH_COUNT = 5
 # Keywords commonly occurring only in Wikipedia internal and meta categories,
 # i.e. "Articles with dead links", "Topic on ..."
-INTERNAL_KEYWORDS = set(['articles', 'category template', 'chapter', 'commons', 'cs1', 'CS1', 'dmy', 'error',
-                         '-language sources', 'mdy', 'move-protected', 'outlines', 'pages', 'semi-protected',
-                         'stubs', 'topics', 'wikidata', 'wikipedia'])
+INTERNAL_CATEGORY_KEYWORDS = set(['articles', 'category template', 'chapter', 'commons', 'cs1', 'CS1', 'dmy', 'error',
+                                  '-language sources', 'mdy', 'move-protected', 'outlines', 'pages', 'semi-protected',
+                                  'stubs', 'topics', 'wikidata', 'wikipedia'])
+# References and such
+META_LINK_KEYWORDS = ['Digital object identifier', 'International Standard Book Number', 'JSTOR', 'Springer']
 # Word matches exclude words of length shorter than this (common words like
 # 'of', 'the' etc.
 MATCHWORD_LENGTH_THRESHOLD = 4
@@ -59,6 +62,7 @@ def get_categories(knewton_path, analyzed_query_count, best_match_query_count, o
         knewton_paths = [knewton_path]
 
     matching_category_output = {}
+    relevant_links_output = {}
 
     for path in knewton_paths:
         keywords = path.split('|')
@@ -68,7 +72,8 @@ def get_categories(knewton_path, analyzed_query_count, best_match_query_count, o
             match_sets.append(wikipedia.search(keywords[-2], results=analyzed_query_count, suggestion=False))
 
         categories = {}
-        category_relevance_score = {}
+        category_relevance_score = defaultdict(int)
+        link_map = {}
 
         match_index = 0
         for matches in match_sets:
@@ -79,10 +84,12 @@ def get_categories(knewton_path, analyzed_query_count, best_match_query_count, o
                     continue
                 except PageError:
                     continue
+                page_title_words = [word.lower() for word in page.title.split()]
                 categories[page.title] = [category for category in page.categories
-                    if not _has_substring_from_set(category, INTERNAL_KEYWORDS)]
-                category_relevance_score[page.title] = _get_category_relevance_score(
-                    categories[page.title], path, MATCH_SET_WEIGHTS[match_index])
+                    if not _has_substring_from_set(category, INTERNAL_CATEGORY_KEYWORDS)]
+                link_map[page.title] = page.links
+                category_relevance_score[page.title] += _get_category_relevance_score(
+                    categories[page.title], page_title_words, path, MATCH_SET_WEIGHTS[match_index])
             match_index += 1
 
         relevant_category_keys = sorted(categories, key=category_relevance_score.get,
@@ -93,11 +100,20 @@ def get_categories(knewton_path, analyzed_query_count, best_match_query_count, o
 
         matching_category_output[path] = []
         click.echo('Categories found for query: %s' % path)
+
+        links = set()
+        linkset_list = []
+        weight_list = []
+
         for page_title in relevant_category_keys:
             category_path = categories[page_title]
+            links.update(link_map[page_title])
+            linkset_list.append(link_map[page_title])
+            weight_list.append(category_relevance_score[page_title])
+
             if len(category_path):
                 categories_found = True
-                message = '\t%s: %s' % (page_title, '|'.join(category_path))
+                message = '\t%s, %.2f: %s' % (page_title, category_relevance_score[page_title], '|'.join(category_path))
                 matching_category_output[path].append(message)
                 click.echo(message)
                 best_match_count_current += 1
@@ -109,12 +125,30 @@ def get_categories(knewton_path, analyzed_query_count, best_match_query_count, o
             matching_category_output[path].append(message)
             click.echo(message)
 
+        relevant_links = _get_relevant_links(links, linkset_list, weight_list)
+        if len(relevant_links):
+            try:
+                message = '%s: %s' % (path, ", ".join(relevant_links))
+                relevant_links_output[path] = message
+                click.echo(message)
+            except UnicodeDecodeError:
+                pass
+
     if len(output_file):
-        with open(output_file, 'w') as output:
+        with open(output_file + '_categories.txt', 'w') as output:
             for key in matching_category_output:
-                output.write('Categories found for query: %s\n' % key)
-                for item in matching_category_output[key]:
-                    output.write(item.encode('utf8') + '\n')
+                try:
+                    output.write('Categories found for query: %s\n' % key.encode('utf8'))
+                    for item in matching_category_output[key]:
+                        output.write(item.encode('utf8') + '\n')
+                except UnicodeDecodeError:
+                    pass
+        with open(output_file + '_relevant_links.txt', 'w') as output:
+            for key in relevant_links_output:
+                try:
+                    output.write(relevant_links_output[key].encode('utf8') + '\n')
+                except UnicodeDecodeError:
+                    pass
 
 
 def _has_substring_from_set(word, wordset):
@@ -131,9 +165,10 @@ def _has_substring_from_set(word, wordset):
     return False
 
 
-def _get_category_relevance_score(category_path, query, weight):
+def _get_category_relevance_score(category_path, page_title_words, query, weight):
     """
     :param category_path: Wikipedia category path
+    :param page_title_words: Words in the title of the page tagged by these categories
     :param query: Submitted query (taxonomy path or concept description)
     :param weight: If relevance score is computed for a higher-level
     category, it's less important (weight is the multiplication factor to
@@ -141,10 +176,10 @@ def _get_category_relevance_score(category_path, query, weight):
     :return: The number of words matching between the Wikipedia category
     path and the submitted query
     """
-    wiki_words = set()
+    wiki_words = set(page_title_words)
     for item in category_path:
-        wiki_words.update(re.split(re.compile('\ '), item))
-    query_words = set(re.split(re.compile('\||\ '), query))
+        wiki_words.update([word.lower() for word in re.split(re.compile('\ '), item)])
+    query_words = set([word.lower() for word in re.split(re.compile('\||\ '), query)])
     return weight * len([word for word in wiki_words.intersection(query_words)
         if len(word) >= MATCHWORD_LENGTH_THRESHOLD])
 
@@ -153,6 +188,35 @@ def _capitalize_internal_keywords(wordset):
     capitalized = [word.title() for word in wordset]
     wordset.update(capitalized)
 
+
+def _get_relevant_links(all_links, link_sets, weights):
+    """
+    :param all_links: set of links to filter
+    :param link_sets: links clustered in sets common to a single article
+    :param weights: weights assigned to these sets of links, based on
+    category relevance
+    :return: links that get at least half of the maximum score, where the
+    max score is the sum of max(1, set_weight), computed over all set_weights
+    """
+    relevant = []
+    max_score = 0
+    for weight in weights:
+        max_score += max(1, weight)
+
+    for link in all_links:
+        index = 0
+        score = 0
+        for link_set in link_sets:
+            if link in link_set:
+                score += max(weights[index], 1)
+            index += 1
+        if score > max_score/2 and not _has_substring_from_set(link, META_LINK_KEYWORDS):
+            relevant.append(link)
+
+    relevant.sort(reverse=True)
+    return relevant
+
+
 if __name__ == '__main__':
-    _capitalize_internal_keywords(INTERNAL_KEYWORDS)
+    _capitalize_internal_keywords(INTERNAL_CATEGORY_KEYWORDS)
     get_categories()
