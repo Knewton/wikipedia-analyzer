59773898cff22906ff3e3e514bfa5f73df5a949a
Alex Heitzmann <alex.heitzmann@knewton.com>
2014-12-12 21:18:03 -0500

    Cleanup wikidump_parser

--- pagefetcher/wikidump_parser.py
+++ pagefetcher/wikidump_parser.py
@@ -1,20 +1,55 @@
 from xml.etree import ElementTree
 
-def xml_convert(path):
-    with open(path , 'rt') as  f:
+def parse(dumpfile):
+    """
+    Parse the xml in the specified dump file.
+
+    This file is expected to conform to a particular
+    schema, summarized below. Such files can be obtained from
+    http://en.m.wikipedia.org/w/index.php?title=Special:Export&action=submit
+    To simplify parsing, you must manually remove the namespaces attribute of the mediawiki
+    element before using this function.
+
+    <mediawiki>
+      <siteinfo>
+        ...
+      </siteinfo>
+      <page>
+        <title>Page Title</title>
+        <id>#########</id>
+        ...
+        <revision>
+          ...
+          <format>text/x-wiki</format>
+          <text>
+            Bla Bla Bla this is wiki markup text
+          </text>
+          ...
+      <page>
+      <page>
+        ...
+      </page>
+      ...
+    </mediawiki>
+
+    :param path: Full path to the xml file
+    :return: An iterable of 3-tuples in the format (str:pageId, str:pageTitle, str:pageText)
+    """
+
+    with open(dumpfile) as f:
         tree = ElementTree.parse(f)
-    #root = tree.getroot()
-    Dlist = []
-    for page in tree.findall('page'):
+
+    for page in tree.iterfind('page'):
         title = page.find('title')
-        id = page.find('id')
+        page_id = page.find('id')
         rev = page.find('revision')
-        text =  rev.find('text')
-        tup = (id.text , title.text , text.text)
-        Dlist.append(tup)
-    return Dlist
+        text = rev.find('text')
+        tup = (page_id.text, title.text, text.text)
+        yield tup
 
-
+if __name__ == "__main__":
+    pages = parse("/Users/alex.heitzmann/Downloads/Wikipedia-20141212192912.xml")
+    print pages.next()
 
 
 
