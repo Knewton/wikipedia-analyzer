5d42869f88dfd3fed11d8e11243e7fbc0078273a
Miroslava Sotakova <mirka@knewton.com>
2014-12-15 16:43:12 -0500

    Including url + id into relevant link output
    Some more comments and minor changes to the algorithm.

--- filters/get_wiki_categories.py
+++ filters/get_wiki_categories.py
@@ -12,6 +12,10 @@ belong to, and then compute the matching-word count with the submitted
 knewton taxonomy path. Based on this count, we assign the relevance score
 to the returned results (pages), and output the best matches'
 assigned categories as the most promising categories to search for.
+3. We collect the links from pages determined to be most relevance by their
+wiki-category classification. We look at the number of occurences of each link
+among the analyzed search query result, and output the ones with the highest
+score as the most relevant for the given Knewton query.
 """
 
 import click
@@ -34,7 +38,7 @@ INTERNAL_CATEGORY_KEYWORDS = set(['articles', 'category template', 'chapter', 'c
                                   '-language sources', 'mdy', 'move-protected', 'outlines', 'pages', 'semi-protected',
                                   'stubs', 'topics', 'wikidata', 'wikipedia'])
 # References and such
-META_LINK_KEYWORDS = ['Digital object identifier', 'International Standard Book Number', 'JSTOR', 'Springer']
+META_LINK_KEYWORDS = ['Digital object identifier', 'International Standard Book Number', 'JSTOR', 'Springer', 'ArXiv']
 # Word matches exclude words of length shorter than this (common words like
 # 'of', 'the' etc.
 MATCHWORD_LENGTH_THRESHOLD = 4
@@ -42,7 +46,15 @@ MATCHWORD_LENGTH_THRESHOLD = 4
 # If the title of the topic deepest in the tree is short, it's more likely to be not very specific. We then
 # try to find the relevant topics based on taxons on the higher taxonomy levels but calculate but consider
 # their relevances with lower weights
-MATCH_SET_WEIGHTS = [1, 0.2]
+MATCH_SET_WEIGHTS = [1, 0.5]
+# When evaluating a linked page relevance across a set of pages, the score the page is assigned is based on
+# the number of occurrences (as a link), and the referencing page relevance.
+CONFIDENCE_THRESHOLD = 0.7
+
+
+# Cache wiki page when retrieving it. It's a slow call.
+page_cache = {}
+
 
 @click.command()
 @click.option('--knewton_path', prompt='Pipe-separated taxonomy path/concept or the file name for batch load',
@@ -56,19 +68,42 @@ MATCH_SET_WEIGHTS = [1, 0.2]
 @click.option('--output_file', default="",
               prompt='Output filename', help='Output filename.')
 def get_categories(knewton_path, analyzed_query_count, best_match_query_count, output_file):
+    """
+    For a given Knewton taxonomy path/concept description, analyze wiki pages that are among
+    the most relevant search results, get a couple of those, refine the analysis by analyzing
+    their assigned categories, and referenced links, then return what seem to be relevant matching
+    wiki categories, and relevant webpage urls.
+    :param knewton_path: Knewton taxonomy path/concept description/a file for a batch of those
+    :param analyzed_query_count: Analyze this many best-match search results for the Knewton query.
+    :param best_match_query_count: Order the returned queries by their assigned categories (in this
+    script, this is based on matching-word count). Claim those to be the best-fit categories. Further
+    analyze links referenced from these pages.
+    :param output_file: If specified, the reports are written into text-files with this prefix.
+    :return:
+    """
+    # Check if knewton_path defines the file, if so, parse the file, if not, consider it a single
+    # query case.
     if os.path.exists(knewton_path):
         knewton_paths = [line.strip() for line in open(knewton_path, 'r').readlines()]
     else:
         knewton_paths = [knewton_path]
 
+    # Dictionary to store matching categories
     matching_category_output = {}
+    # Dictionary to store relevant links
     relevant_links_output = {}
 
     for path in knewton_paths:
         keywords = path.split('|')
+        # If knewton_path defines a taxon, the last element on the patch (the most specific taxon)
+        # is supposedly most accurate to use for the search, while being not overly restrictive
+        # (which the whole path with its encoding could be)
         match_sets = [wikipedia.search(keywords[-1], results=analyzed_query_count,
                                        suggestion=False)]
-        if len(keywords) > 1 and len(keywords[-2].split()) > len(keywords[-1].split()):
+        # If the deepest taxons is too short (e.g. "series"), it may be too ambiguous, and to get
+        # accurate results, we may need to refine the context. Try to search by the last-but-one
+        # element on the path, which is less-specific category and thus considered with lower weight
+        if len(keywords) > 1 and len(keywords[-1].split()) == 1:
             match_sets.append(wikipedia.search(keywords[-2], results=analyzed_query_count, suggestion=False))
 
         categories = {}
@@ -76,22 +111,40 @@ def get_categories(knewton_path, analyzed_query_count, best_match_query_count, o
         link_map = {}
 
         match_index = 0
+
+        # For each search query, we get a set of best-match results. For each of these sets, let analyze
+        # the pages further.
         for matches in match_sets:
+            # Returned search results are presumably returned sorted by relevance (however, we're not
+            # sure of the context). Give this order some weight by using the result_score (decreasing from
+            # len(matches) -> 1) as a multiplicative factor when calculating the category relevance score.
+            result_score = len(matches)
             for match in matches:
                 try:
                     page = wikipedia.page(match)
+                    page_cache[page.title] = page
                 except DisambiguationError:
                     continue
                 except PageError:
                     continue
+                # Tokenize the page title
                 page_title_words = [word.lower() for word in page.title.split()]
+                # Get cateogories for each page but skip the internal/meta wiki categories
                 categories[page.title] = [category for category in page.categories
                     if not _has_substring_from_set(category, INTERNAL_CATEGORY_KEYWORDS)]
+                # Save page links
                 link_map[page.title] = page.links
-                category_relevance_score[page.title] += _get_category_relevance_score(
+                # Assign relevance score to the categories assigned to this page, based on word-match
+                # between the words occurring in the page title + wiki category path vs Knewton path
+                # If the page was obtained as a search result for a higher-level category, consider
+                # it with a lower weight
+                category_relevance_score[page.title] += result_score * _get_category_relevance_score(
                     categories[page.title], page_title_words, path, MATCH_SET_WEIGHTS[match_index])
+
+                result_score -= 1
             match_index += 1
 
+        # Sort page-titles (keys of "cateogories" map) by their assigned score
         relevant_category_keys = sorted(categories, key=category_relevance_score.get,
                                         reverse=True)
 
@@ -106,12 +159,16 @@ def get_categories(knewton_path, analyzed_query_count, best_match_query_count, o
         weight_list = []
 
         for page_title in relevant_category_keys:
+            # Analyze best_match_count page_titles to gather relevant links.
+            # Gather links from the page, and add the page itself, build a set of these tokens
+            # for each page analyzed.
             category_path = categories[page_title]
             links.update(link_map[page_title])
             links.add(page_title)
             linkset_list.append(link_map[page_title] + [page_title])
             weight_list.append(category_relevance_score[page_title])
 
+            # Output formatting
             if len(category_path):
                 categories_found = True
                 message = '\t%s, %.2f: %s' % (page_title, category_relevance_score[page_title], '|'.join(category_path))
@@ -126,14 +183,20 @@ def get_categories(knewton_path, analyzed_query_count, best_match_query_count, o
             matching_category_output[path].append(message)
             click.echo(message)
 
+        # Get links from the analyzed pages, evaluated as relevant.
         relevant_links = _get_relevant_links(links, linkset_list, weight_list)
-        if len(relevant_links):
+        relevant_link_data = _map_titles_to_urls_ids(relevant_links)
+
+        message = path + ':\n'
+        for link in relevant_link_data:
+            data = relevant_link_data[link]
             try:
-                message = '%s: %s' % (path, ", ".join(relevant_links))
-                relevant_links_output[path] = message
-                click.echo(message)
+                message += '\t%s: %s\n' % (data.url, data.pageid)
             except UnicodeDecodeError:
-                pass
+                continue
+
+        relevant_links_output[path] = message
+        click.echo(message)
 
     if len(output_file):
         with open(output_file + '_categories.txt', 'w') as output:
@@ -154,6 +217,7 @@ def get_categories(knewton_path, analyzed_query_count, best_match_query_count, o
 
 def _has_substring_from_set(word, wordset):
     """
+    Checks if a given string contains a substring from a given set.
     :param word: Word to examine
     :param wordset: Set of words we are interested if they
     occur in the given word
@@ -168,6 +232,8 @@ def _has_substring_from_set(word, wordset):
 
 def _get_category_relevance_score(category_path, page_title_words, query, weight):
     """
+    Try to score category relevance according to the number of words above certain
+    length it shares with Knewton taxonomy path/concept description.
     :param category_path: Wikipedia category path
     :param page_title_words: Words in the title of the page tagged by these categories
     :param query: Submitted query (taxonomy path or concept description)
@@ -192,6 +258,8 @@ def _capitalize_internal_keywords(wordset):
 
 def _get_relevant_links(all_links, link_sets, weights):
     """
+    Get links that are significant for a set of link-sets gathered
+    from several wiki pages.
     :param all_links: set of links to filter
     :param link_sets: links clustered in sets common to a single article
     :param weights: weights assigned to these sets of links, based on
@@ -201,6 +269,18 @@ def _get_relevant_links(all_links, link_sets, weights):
     """
     relevant = []
     max_score = 0
+
+    # Compute a score for each link referenced from some of the analyzed
+    # pages + the analyze page titles themselves. This is done as follows:
+    # For each reachable page (within one-hop from the starting set), start with 0.
+    # If a page has relevance_score r >= 1 based on category word_match, add r to the
+    # scores of all links, and the page itself. If it has score <1 (in the current model,
+    # ==0), add 1 to each link/the page itsef - This is because even though there's no
+    # word match between the categories assigned and Knewton query, the page was returned
+    # as a search result for some reason.
+    # Obviously, the maximum score is achieved for a page referenced from or identical to
+    # each from the analyzed pages.
+    # Consider a link relevant if it has at least 1/2 of the maximum score.
     for weight in weights:
         max_score += max(1, weight)
 
@@ -211,11 +291,37 @@ def _get_relevant_links(all_links, link_sets, weights):
             if link in link_set:
                 score += max(weights[index], 1)
             index += 1
-        if score > max_score/2 and not _has_substring_from_set(link, META_LINK_KEYWORDS):
+        if score > max_score * CONFIDENCE_THRESHOLD and not _has_substring_from_set(link, META_LINK_KEYWORDS):
             relevant.append(link)
 
     relevant.sort(reverse=True)
     return relevant
+
+
+def _map_titles_to_urls_ids(titles):
+    """
+    Convert a set of wiki page titles into the corresponding (url, id) pairs.
+    """
+    link_data = {}
+    for title in titles:
+        if title in page_cache:
+            page = page_cache[title]
+        else:
+            try:
+                page = wikipedia.page(title)
+                page_cache[title] = page
+            except DisambiguationError:
+                continue
+            except PageError:
+                continue
+        link_data[title] = LinkData(page.url, page.pageid)
+    return link_data
+
+
+class LinkData:
+    def __init__(self, url, pageid):
+        self.url = url
+        self.pageid = pageid
 
 
 if __name__ == '__main__':
